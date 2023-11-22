7265c1549600955152acb2587b0de7fc29f185c8
Araf Al Jami <clown1331@gmail.com>
2023-11-22 19:47:52 +0000

    Update

---
+++ description.md
@@ -0,0 +1,17 @@
+So, Alice is starting a new company, and she is in the market for office space to rent for her company.
+
+She decided to go with WoWerk, a commercial real estate company that provides shared workspaces for technology startups and services for other enterprises.
+
+WoWerk, leases a building and sub-divides each floor to smaller section to rent it out to other companies.
+
+She is expecting fast growth for her company, and so will need a large office space to accommodate future expansion.
+
+Help her find the largest office space she can rent for each floor.
+
+# **Input**
+Input starts with an integer T ( ≤ 10), denoting the number of floor.
+
+Each case starts with a line containing two integers: m and n (1 ≤ m, n ≤ 2000). Each of the next m lines contains n characters (either 0 or 1) denoting floor or wall.
+
+# **Output**
+For each floor, print the floor number and the maximum office space Alice could rent.
---
+++ run.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+g++ gen.cpp -o gen
+echo generator compiled
+rm -rf Input Output testcase.zip
+mkdir -p Input
+mkdir -p Output
+# exe, filename, seed, t_limit, n_limit, m_limit
+# sample
+./gen Input/input1.txt 137 3 5 5
+./gen Input/input2.txt 452 3 5 5
+./gen Input/input3.txt 5457 5 5 5
+echo sample input generated
+./gen Input/input4.txt 1347 5 25 25
+./gen Input/input5.txt 425 5 50 50
+./gen Input/input6.txt 557 5 100 100
+echo small input generated
+./gen Input/input7.txt 13347 10 2000 2000
+./gen Input/input8.txt 42325 10 2000 2000
+./gen Input/input9.txt 55357 10 2000 2000
+echo medium input generated
+g++ sol.cpp -o sol
+echo solution compiled
+# sample
+./sol < Input/input1.txt > Output/output1.txt
+./sol < Input/input2.txt > Output/output2.txt
+./sol < Input/input3.txt > Output/output3.txt
+echo sample output generated
+./sol < Input/input4.txt > Output/output4.txt
+./sol < Input/input5.txt > Output/output5.txt
+./sol < Input/input6.txt > Output/output6.txt
+echo small output generated
+./sol < Input/input7.txt > Output/output7.txt
+./sol < Input/input8.txt > Output/output8.txt
+./sol < Input/input9.txt > Output/output9.txt
+echo medium output generated
+zip testcase.zip Input/* Output/* Config.json
+echo testcase zipped
+
+
+echo "#**Sample Input**" > sample.md
+echo "\`\`\`" >> sample.md
+cat Input/input1.txt >> sample.md
+echo "\`\`\`" >> sample.md
+echo "\`\`\`" >> sample.md
+cat Input/input2.txt >> sample.md
+echo "\`\`\`" >> sample.md
+echo "\`\`\`" >> sample.md
+cat Input/input3.txt >> sample.md
+echo "\`\`\`" >> sample.md
+
+echo "" >> sample.md
+echo "#**Sample Output**" >> sample.md
+echo "\`\`\`" >> sample.md
+cat Output/output1.txt >> sample.md
+echo "\`\`\`" >> sample.md
+echo "\`\`\`" >> sample.md
+cat Output/output2.txt >> sample.md
+echo "\`\`\`" >> sample.md
+echo "\`\`\`" >> sample.md
+cat Output/output3.txt >> sample.md
+echo "\`\`\`" >> sample.md
